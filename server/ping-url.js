8066c439f0318d624528f2d0f5a185baf77c2942
akshatnema <20bcs022@iiitdmj.ac.in>
2022-10-05 21:47:23 +0530

    Ping message added

--- server/ping-url.js
+++ server/ping-url.js
@@ -1,11 +1,12 @@
 const ping = require("ping");
 
-async function pingUrl () {
+async function pingUrl() {
     let res = await ping.promise.probe(process.env.URL, {
         timeout: 10
-      });
-    if(!res.alive) console.log(res);
+    });
+    if (!res.alive) console.log(res);
+    else console.log("Ping successful")
     return res.alive;
 }
 
-module.exports = {pingUrl};
+module.exports = { pingUrl };
